3be7206c519074b918d38aef6347b2a0fda46117
sarahduv <sarah.duvdevani@gmail.com>
2019-10-19 18:47:37 -0700

    lint and tests are passing

---
+++ __tests__/models-modular/products-modular.test.js
@@ -0,0 +1,61 @@
+const Products = require('../../models-modular/products/products.js');
+let products = new Products();
+
+require('../supergoose.js');
+
+
+describe('Products Model (Modular)', () => {
+
+  beforeEach(async () => {
+    await products.deleteMany({});
+  });
+
+  // How will you handle both the happy path and edge cases in these tests?
+
+  it('can create() a new product', async () => {
+    let obj = { _id: '10', name: 'John', description: 'Person' };
+    
+    const record = await products.create(obj);
+    Object.keys(obj).forEach(key => {
+      expect(record[key]).toEqual(obj[key]);
+    });  
+  });
+
+  it('can get() a product', async () => {
+    let obj = { _id: '123', name: 'John', description: 'Person' };
+    await products.create(obj);
+    const record = await products.get({_id: obj._id});
+    Object.keys(obj).forEach(key => {
+      expect(record[key]).toEqual(obj[key]);
+    });
+  });
+
+
+  it('can get() all products', async () => {
+    let obj1 = { _id: '1', name: 'John', description: 'Person' };
+    let obj2 = { _id: '2', name: 'John2', description: 'Person2' };
+    
+    await products.create(obj1);
+    await products.create(obj2);
+
+    const records = await products.get({});
+    expect(records.count).toEqual(2);
+  });
+
+  it('can update() a product', async () => {
+    let obj = { _id: '123', name: 'John', description: 'Person' };
+    await products.create(obj);
+    await products.update('123', {name: 'Tarzan'});
+    const record = await products.get({_id: obj._id});
+    expect(record.name).toEqual('Tarzan');
+  });
+
+  it('can delete() a product', async () => {
+    let obj = { _id: '123', name: 'John', description: 'Person' };
+    await products.create(obj);
+    await products.delete('123');
+    const record = await products.get({_id: obj._id});
+    expect(record.count).toEqual(0);
+  });
+
+});
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Require your model
+
+// Mongoose Server URI
+const MONGOOSE_URI = 'mongodb://localhost:27017/class05';
+
+// Connect
+mongoose.connect(MONGOOSE_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// Do some work
+const Categories = require('./models-singular/categories.js');
+const categories = new Categories();
+async function doWork() {
+  await categories.delete(9);
+  await categories.create(
+    {_id: '9', name: 'John', description: 'person' }
+  );
+  await categories.update(9,
+    {name: 'Peach'}
+  );
+
+  const fetched = await categories.get(9);
+  console.log(fetched);
+}
+
+doWork().then(() => {
+  // Disconnect
+  mongoose.disconnect();
+  console.log('disconnect');
+}).catch(err => {
+  console.log('Error: ',err);
+  mongoose.disconnect();
+  console.log('disconnect');
+});
+
+
+
+/*
+categories.create({_id: '5', name: 'John', description: 'person' }).then(() => {
+  categories.get(5).then( (result) => {
+    console.log(result);
+
+    // Disconnect
+    mongoose.disconnect();
+    console.log('disconnect');
+  });
+});
+*/
